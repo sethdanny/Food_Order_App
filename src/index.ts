b3b018c26ef59714281c3c174a890ca542ce619a
sethdanny <naddulidaniel1994@gmail.com>
2023-10-27 17:45:16 +0300

    created admin routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import {VandorRoute} from './routes/VandorRoute';
+import {AdminRoute} from './routes/AdminRoute';
 
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/admin', AdminRoute);
 app.use('/vendor', VandorRoute);
 
 app.listen(3000, () => {
--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -1,1 +1,10 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.json({message : 'Hello Admin'});
+});
+
+
+export {router as AdminRoute};
